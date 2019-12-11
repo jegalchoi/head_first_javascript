707f89a6dba56043aef7cf7a1f639fe011ad640a
jegalchoi <thefreshbegal@gmail.com>
2019-12-11 15:35:46 -0800

    Complete Battleship my version

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -1,34 +1,62 @@
-let location1 = Math.floor(Math.random() * 5);
-let location2 = location1 + 1;
-let location3 = location1 + 2;
+function Location(num, coordinate) {
+  this[num] = coordinate;
+  this.hit = false;
+}
 
-let guess;
-let hits = 0;
-let guesses = 0;
+function Ship(lengthOfShip) {
+  this.guess;
+  this.hits = 0;
+  this.guesses = 0;
+  this.isSunk = false;
+  this.locations = [];
 
-let isSunk = false;
+  let randomLoc = Math.floor(Math.random() * 5);
+  
+  for (let i = 1; i <= lengthOfShip; i += 1) {
+    this.locations.push(new Location(i, randomLoc + i))
+  }
 
-while (!isSunk) {
+}
+
+let ship = new Ship(3);
+
+while (!ship.isSunk) {
   guess = prompt('Ready, aim, fire! (enter a number from 0-6): ');
 
   if (guess < 0 || guess > 6) {
     alert("Please enter a valid number!");
   } else {
-    guesses += 1;
+    ship.guesses += 1;
   }
 
-  if (guess == location1 || guess == location2 || guess == location3) {
-    hits += 1;
-    alert('Hit!');
+  let guessLocation;
+  let guessHit;
 
-    if (hits == 3) {
-      isSunk = true;
+  for (i = 1; i < ship.locations.length + 1; i += 1) {
+    if (ship.locations[i - 1][i] == +guess) {
+      guessLocation = ship.locations[i - 1][i];
+      guessHit = ship.locations[i - 1].hit;
+    }
+    if (guessLocation != undefined) {
+      break;
+    }
+  }
+
+  if (!guessHit && guess == guessLocation) {
+    alert('Hit!');
+    ship.hits += 1;
+    guessHit = true;
+
+    if (ship.hits == 3) {
+      ship.isSunk = true;
       alert("You sunk my battleship!");
     }
+  } else if (guessHit && guess == guessLocation) {
+    alert('You already guessed that coordinate!');
   } else {
     alert('Miss!');
-  } 
+  }
 }
 
-let stats = `You took ${guesses} guesses to sink the battleship, which means your accuracy was ${3 / guesses * 100}%.`
+let stats = `You took ${ship.guesses} guesses to sink the battleship, which means your accuracy was ${3 / ship.guesses * 100}%.`
 alert(stats);
